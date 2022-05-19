0b1417157120d03d31744d6f16d8946c03a859a7
JasonA <74770401+JasonA-dev@users.noreply.github.com>
2022-05-19 16:10:55 +0200

    updated Verilator

---
+++ verilator/verilate.sh
@@ -0,0 +1,15 @@
+verilator \
+-cc -exe --public --trace --savable \
+--compiler msvc +define+SIMULATION=1 \
+-O3 --x-assign fast --x-initial fast --noassert \
+--converge-limit 6000 \
+-Wno-fatal \
+--top-module top sim.v \
+../rtl/audio.v \
+../rtl/dma.v \
+../rtl/ram88.v \
+../rtl/rom.v \
+../rtl/video.v \
+../rtl/65c02/ALU.v \
+../rtl/65c02/cpu_65c02.v \
+../rtl/65c02/cpu.v
